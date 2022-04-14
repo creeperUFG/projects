556d4a322e1d0bf398f21f3428bc18460f80751c
Fernando Severino <fernandosevjipa@gmail.com>
2022-04-14 09:15:03 -0300

    Fix project

--- src/controllers/ProjectsController.ts
+++ src/controllers/ProjectsController.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import * as express from "express";
 const Projects = require("./../models/Projects");
 
 interface ProjectsDTO {
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import * as express from "express";
 
 const ProjectsController = require("./controllers/ProjectsController");
 
